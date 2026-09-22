e2a3b52f584f96666d08319dffa3993827c46081
Rulia914 <falconejulie@gmail.com>
2026-09-22 14:49:32 +0200

    Intégration des templates

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import TodoList from "./components/todoList/TodoList";
 
   new TodoList({
     el: "#app",
-    title:"My amazing Todolist",
+    title:"My Todolist",
     apiUrl: "https://6a4f4934f45d5352b6112e4b.mockapi.io",
   }).render();
